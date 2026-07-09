52be242a6900129ddb17cce36bf699aab68f688b
swaroopamanthena2004-ui <swaroopamanthena2004@gmail.com>
2026-07-09 09:23:02 +0530

    Dependency frequency change to L

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Sample endpoint returning DevOps metrics data
 app.get('/api/metrics', (req, res) => {
     res.json({
-        deploymentFrequency: "Lightening Fast Delivery",
+        deploymentFrequency: "L",
         leadTimeForChanges: "< 30 mins",
         timeToRestoreService: "< 5 mins",
         changeFailRate: "< 1%"
